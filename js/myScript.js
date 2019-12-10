48ee3fa3708046cb7dec464adbd8327131c99ecb
Evian <894379096@qq.com>
2019-12-10 20:51:30 +0800

    改进

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,17 @@
 // JavaScript Document
-$(window).load(function(){
-        $('#slider').nivoSlider();
+'use strict';
+
+$(function(){
+    $('#slider').nivoSlider();
 });
 
 setInterval(function() {
       var now = (new Date()).toLocaleString();
       $('#show').text(now);
   }, 1000);
+
+$(function () {
+    $('.btn-group :button').click(function () {        
+        $(".btn-content .aaa").eq($(this).index()).show().siblings().hide();
+    })
+});
